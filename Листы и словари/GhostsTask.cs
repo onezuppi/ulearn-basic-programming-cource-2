327e13415ba69e34a33fdf34bd00c9c7c7d41d07
onezuppi <52073821+onezuppi@users.noreply.github.com>
2021-06-26 20:54:29 +0500

    Added 'Листы и словари'.

---
+++ Листы и словари/GhostsTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace hashes
+{
+    public class GhostsTask :
+        IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>,
+        IMagic
+    {
+        private readonly byte[] content;
+        private readonly Vector vector;
+        private readonly Segment segment;
+        private readonly Cat cat;
+        private readonly Document document;
+        private readonly Robot robot;
+
+        public GhostsTask()
+        {
+            var encoding = Encoding.UTF8;
+            content = encoding.GetBytes("abcdefghijklmnopqrstuvwxyz");
+            vector = new Vector(0, 0);
+            segment = new Segment(vector, vector);
+            cat = new Cat("Pop", "Mixed", DateTime.Now);
+            document = new Document("new", encoding, content);
+            robot = new Robot("rd-d2");
+        }
+
+        Vector IFactory<Vector>.Create() => vector;
+
+        Segment IFactory<Segment>.Create() => segment;
+
+        Cat IFactory<Cat>.Create() => cat;
+
+        Document IFactory<Document>.Create() => document;
+
+        Robot IFactory<Robot>.Create() => robot;
+
+        public void DoMagic()
+        {
+            content[0]++;
+            vector.Add(new Vector(0, 1));
+            segment.End.Add(new Vector(0, 2));
+            cat.Rename("Pop-pop");
+            Robot.BatteryCapacity += 1;
+        }
+    }
+}
---
+++ Листы и словари/ReadonlyBytes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hashes
+{
+    public class ReadonlyBytes : IEnumerable<byte>
+    {
+        private const int PrimeNumber = 16777619;
+        private readonly byte[] values;
+        private (int Value, bool IsCalсulated) hash;
+
+        public ReadonlyBytes(params byte[] bytes)
+        {
+            values = bytes ?? throw new ArgumentNullException();
+            hash = (0, false);
+        }
+
+        public int Length => values.Length;
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator<byte> GetEnumerator() => ((IEnumerable<byte>) values).GetEnumerator();
+
+        public override string ToString() => $"[{string.Join(", ", values)}]";
+
+        public override bool Equals(object obj)
+        {
+            var point = obj as ReadonlyBytes;
+            if (point == null || point.Length != values.Length || obj.GetType() != GetType())
+                return false;
+                
+            return point == this || !point.Where((item, index) => values[index] != item).Any();
+        }
+
+        public override int GetHashCode()
+        {
+            if (hash.IsCalсulated)
+                return hash.Value;
+            unchecked
+            {
+                hash.Value = values.Aggregate(hash.Value, (r, o) => (r ^ o.GetHashCode()) * PrimeNumber);
+            }
+            hash.IsCalсulated = true;
+
+            return hash.Value;
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= values.Length)
+                    throw new IndexOutOfRangeException();
+
+                return values[index];
+            }
+        }
+    }
+}
